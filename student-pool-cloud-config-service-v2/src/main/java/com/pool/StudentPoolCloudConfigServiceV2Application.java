46184bd9a22769e2b12d858dfe034356110ac539
shiva508 <dasarishiva1@gmail.com>
2021-05-22 15:51:17 +0530

    cloud config

---
+++ student-pool-cloud-config-service-v2/src/main/java/com/pool/StudentPoolCloudConfigServiceV2Application.java
@@ -0,0 +1,13 @@
+package com.pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentPoolCloudConfigServiceV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentPoolCloudConfigServiceV2Application.class, args);
+	}
+
+}
---
+++ student-pool-global-config-service-v2/src/main/java/com/pool/StudentPoolGlobalConfigServiceV2Application.java
@@ -0,0 +1,13 @@
+package com.pool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentPoolGlobalConfigServiceV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentPoolGlobalConfigServiceV2Application.class, args);
+	}
+
+}
