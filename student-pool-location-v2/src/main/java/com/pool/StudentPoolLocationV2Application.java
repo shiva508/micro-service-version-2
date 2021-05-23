4873c354690c1d832dbc84f0919ce9efbc9b0de4
shiva508 <dasarishiva1@gmail.com>
2021-05-23 09:27:40 +0530

    final changes

--- student-pool-location-v2/src/main/java/com/pool/StudentPoolLocationV2Application.java
+++ student-pool-location-v2/src/main/java/com/pool/StudentPoolLocationV2Application.java
@@ -2,10 +2,12 @@ package com.pool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class StudentPoolLocationV2Application {
 
 	public static void main(String[] args) {
--- student-pool-location-v2/src/main/java/com/pool/controller/ProxyController.java
+++ student-pool-location-v2/src/main/java/com/pool/controller/ProxyController.java
@@ -11,18 +11,20 @@ import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 public class ProxyController {
-	private Logger logger=LoggerFactory.getLogger(ProxyController.class);
+	private Logger logger = LoggerFactory.getLogger(ProxyController.class);
+
 	@GetMapping("/circutebreakertest")
-	//@Retry(name = "cricutebreaker-api",fallbackMethod = "somthingWentWrong")
-	@CircuitBreaker(name = "default",fallbackMethod = "somthingWentWrong")
+	// @Retry(name = "cricutebreaker-api",fallbackMethod = "somthingWentWrong")
+	@CircuitBreaker(name = "default", fallbackMethod = "somthingWentWrong")
 	public String getSampleCircuteBreakerData() {
-		logger.info("Circute Break method ","Called");
-		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+		logger.info("Circute Break method ", "Called");
+		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url",
+				String.class);
 		return "Circute Breaker Data";
 	}
-	
+
 	public String somthingWentWrong(Exception ex) {
-		
+
 		return "Something really went wrong";
 	}
 }
