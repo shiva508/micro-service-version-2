bbcf27b04e7ea5fabccb3bb108ded8ea294bdf3c
shiva508 <dasarishiva1@gmail.com>
2021-08-02 20:36:58 +0530

    xml/json data

--- studentpool-profile/src/main/java/com/pool/controller/userprofile/UserProfileController.java
+++ studentpool-profile/src/main/java/com/pool/controller/userprofile/UserProfileController.java
@@ -1,11 +1,10 @@
 package com.pool.controller.userprofile;
 
 import java.util.ArrayList;
-import java.util.List;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,9 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.pool.form.response.CommonResponse;
-import com.pool.modal.KafkaUserprofile;
 import com.pool.modal.Userprofile;
-import com.pool.service.kafkauserprofile.KafkaUserProfileService;
 import com.pool.service.userprofile.UserProfileService;
 
 @RestController
@@ -26,21 +23,16 @@ public class UserProfileController {
 
 	@Autowired
 	private UserProfileService userProfileService;
-	@Autowired
-	private KafkaTemplate<String, Object> kafkaTemplate;
 
-	@Autowired
-	private KafkaUserProfileService kafkaUserProfileService;
 	List<String> data = new ArrayList<String>();
 	Userprofile  userProfile=null;
 
 	@PostMapping("/save")
 	public Userprofile saveUserProfile(@RequestBody Userprofile userprofile) {
-		kafkaTemplate.send("studentpool", userprofile);
 		return userProfileService.saveUserProfile(userprofile);
 	}
 
-	@GetMapping("/userprofiles")
+	@GetMapping(value="/userprofiles",produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 	public List<Userprofile> getUserProfiles() {
 		return userProfileService.getUserProfiles();
 	}
@@ -62,29 +54,7 @@ public class UserProfileController {
 
 	@GetMapping("/publish/{data}")
 	public String publishMessage(@PathVariable("data") String data) {
-		kafkaTemplate.send("studentpool", "HI " + data);
 		return "PUBLISHED";
-	}
-
-	@KafkaListener(groupId = "studentpool-2", topics = "studentpool", containerFactory = "userConcurrentKafkaListenerContainerFactory")
-	public Userprofile consumer(Userprofile userprofileOp) {
-		userProfile=userprofileOp;
-		return userProfile;
-	}
-
-	@KafkaListener(groupId = "studentpool-1", topics = "studentpool", containerFactory = "concurrentKafkaListenerContainerFactory")
-	public List<String> consumerUser(String opdata) {
-		data.add(opdata);
-		return data;
-	}
-	
-	@GetMapping("/stringdata")
-	public List<String> getStringData(){
-		return data;
-	}
-	@GetMapping("/userkafka")
-	public Userprofile getStringDataUser(){
-		return userProfile;
 	}
 	
 }
